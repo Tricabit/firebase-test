9927bde166f750d37f29928564a16273275e8022
Marius Trica <m.trica@palmabit.com>
2022-06-11 19:42:23 +0200

    feat: read/write from firebase realtime db

---
+++ pages/index.tsx
@@ -0,0 +1,66 @@
+import type { NextPage } from "next";
+import { getDatabase, onValue, ref, query } from "@firebase/database";
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import writeData from "../src/firebase/writeData";
+import initFirebase from "../src/firebase";
+
+const Home: NextPage = () => {
+  const [someText, setSomeText] = useState<string>("");
+  const [textData, setTextData] = useState<any[]>([]);
+  useEffect(() => {
+    initFirebase();
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    const db = getDatabase();
+    const starCountRef = ref(db, "testData");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      setTextData(
+        Object.entries(data).map(([key, value]: [key: string, value: any]) => ({
+          key,
+          text: value?.text,
+        }))
+      );
+    });
+  };
+
+  console.log("textData", textData);
+
+  return (
+    <div className={styles.container}>
+      <form action="">
+        <div style={{ display: "flex", flexDirection: "column", width: "50%" }}>
+          <label htmlFor="">Type something...</label>
+          <input
+            type="text"
+            placeholder="Type something..."
+            value={someText}
+            onChange={(e) => setSomeText(e.target.value)}
+          />
+        </div>
+        <button
+          type="button"
+          style={{ marginTop: 20 }}
+          onClick={() => {
+            writeData(someText), setSomeText("");
+          }}
+        >
+          Click me
+        </button>
+      </form>
+      <section style={{ marginTop: 25 }}>
+        {textData.map((el, idx) => (
+          <div key={idx}>
+            <h3>{el.key}</h3>
+            <p>{el.text}</p>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default Home;
